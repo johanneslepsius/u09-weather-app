c51f3c25922611e37ba78ad5e77b6957ba6b3339
johanneslepsius <johannes.lepsius@chasacademy.se>
2021-05-26 15:56:06 +0200

    move root dir

---
+++ backend/index.php
@@ -0,0 +1,17 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$citiesJSON = file_get_contents('./city.list.json');
+$cities = json_decode($citiesJSON, true);
+
+$query = $_GET['query'];
+$citiesResponse = [];
+
+if ($query) {
+    foreach ($cities as $city => $value) {
+        if (stripos($value["name"], $query) !== false) {
+            array_push($citiesResponse, ['name' => $value["name"], 'country' => $value["country"], 'geolocation' => ['latitude' => $value['coord']['lat'], 'longitude' => $value['coord']['lon']]]);
+        };
+    };
+};
+
+echo (json_encode($citiesResponse));
