f1d8cff34a77b1dd0a31cd6e87212a7265ad761c
johanneslepsius <johannes.lepsius@chasacademy.se>
2021-06-14 13:55:44 +0200

    fix search functionality problems

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ A simple weather app, built with React (and a teeny tiny bit of PHP), using the 
 
 2. cd into backend, run php -S localhost:8000 to spin up a local backend server
 
-3. cd into frontend & create a .env file that contains your openweathermap app id in REACT_APP_WEATHER_KEY=
+3. cd into frontend & create a .env file that contains
+
+   - your openweathermap app id in REACT_APP_WEATHER_KEY=
+   - REACT_APP_BASE_URL=https://api.openweathermap.org/data/2.5/onecall
 
 4. also in frontend, open a new terminal and run npm start
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import axios from 'axios';
 import {Weatherinfo} from './weatherinfo/Weatherinfo';
 import { ReactComponent as Pentagram } from "./pentagram.svg";
+import { ReactComponent as Searchbtn } from "./search.svg";
 
 const weatherReducer = (state, action) => {
   switch (action.type) {
@@ -30,26 +31,11 @@ const weatherReducer = (state, action) => {
 
 
 function App() {
-// do i need async?
-  function getWeather (geolocation, units) {
-    // when removing the if stmt some /&¤%¤&)^^^** function(probably line 94) calls this with undefined
-    if (geolocation.latitude){
-      axios(`https://api.openweathermap.org/data/2.5/onecall?lat=${geolocation.latitude}&lon=${geolocation.longitude}&exclude=minutely&units=${units}&appid=${process.env.REACT_APP_WEATHER_KEY}`)
-      .then(result => {
-          dispatchWeather({
-            type: 'WEATHER_FETCH_SUCCESS',
-            isLoading: false,
-            payload: result.data
-          })
-        })
-        .catch(() => 
-          dispatchWeather({type: 'WEATHER_FETCH_ERROR'})
-        );
-      };
-    }
-
+  // 
   const [curr_position, setCurr_position] = React.useState({});
-
+  // the current position
+  // let curr_position = {};
+  // the units to display for the user (not for data fetching)
   const [units, setUnits] = React.useState({temp: '°C', wind: 'km/h'});
 
   const [weather, dispatchWeather] = React.useReducer(
@@ -57,12 +43,32 @@ function App() {
     {data: {}, isLoading: true, isError: false}
   );
 
+  // async await or then?
+  function getWeather (geolocation, units) {
+    if (geolocation.latitude){
+      axios(process.env.REACT_APP_BASE_URL + `?lat=${geolocation.latitude}&lon=${geolocation.longitude}&exclude=minutely&units=${units}&appid=${process.env.REACT_APP_WEATHER_KEY}`)
+      .then(result => {
+        dispatchWeather({
+          type: 'WEATHER_FETCH_SUCCESS',
+          isLoading: false,
+          payload: result.data
+        })
+        })
+      .catch(() => 
+        dispatchWeather({type: 'WEATHER_FETCH_ERROR'})
+      );
+    };
+  };
+
+  
+// this is where the app starts when loading first
   React.useEffect(() => {
     dispatchWeather({type: 'WEATHER_FETCH_INIT'});
 
+    // callback functions for getting position from the  browsers´ geolocation api
     const positionSuccess = position => {
       setCurr_position(position.coords);
-      getWeather(position.coords, 'metric');
+      getWeather(curr_position, 'metric');
     };
 
     const positionError = error => {
@@ -72,15 +78,17 @@ function App() {
     navigator.geolocation.getCurrentPosition(positionSuccess, positionError);
   }, []);
 
+  // false is metric, true is imperial units, could be a simple variable, i know, but only works as state
   const [temptoggle, setTemptoggle] = React.useState(false);
-  // const [temptoggle, toggleTemp] = React.useReducer(previousTemp => !previousTemp, false)
+
+  // units to use for api call. could be a simple variable, i know, but only works as state
   const [newUnits, setNewUnits] = React.useState('metric');
 
   const triggertoggle = () => {
     setTemptoggle(temptoggle => !temptoggle );
     setNewUnits(temptoggle ? ('imperial') : ('metric'));
   }
-  // do i need useeffect here?? only way i found to make it work properly
+
   React.useEffect(() => {
     setNewUnits(temptoggle ? ('imperial') : ('metric'));
   }, [temptoggle]);
@@ -92,44 +100,103 @@ function App() {
       setUnits({temp: '°F', wind: 'mph'});
     };
     dispatchWeather({type: 'WEATHER_FETCH_INIT'});
-    // something calls getweather with undefined position when starting
     getWeather(curr_position, newUnits);
-    // react wants me to add curr_position to dependency array, but i dont want to
-    //  run this code when currposition is updated?? sooo ?
+    // react lint wants me to add curr_position to dependency array, but i dont want to
+    //  run this effect when curr_position is updated?? sooo ?
   }, [newUnits]);
 
   const [city, setCity] = React.useState();
+  const [searchTerm, setSearchTerm] = React.useState('');
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    const spacePosition = searchTerm.indexOf(' ');
-    const actualSearchTerm = searchTerm.substring(0, spacePosition);
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setDisableSearch(true)
     setCity(searchTerm);
+    console.log({searchTerm})
+    const spacePosition = searchTerm.lastIndexOf(' ');
+    const actualSearchTerm = searchTerm.substring(0, spacePosition);
+    // debugger;
     const chosenCity = suggestions.find(el => el.name === actualSearchTerm);
-    console.log(newUnits, "search");
-    dispatchWeather({type: 'WEATHER_FETCH_INIT'});
     setCurr_position(chosenCity.geolocation);
-    getWeather(chosenCity.geolocation, newUnits);
+    console.log(curr_position);
+    // let chosenCity;
+    // async function chooseCity() {
+      // const chosenCity = suggestions.find(el => el.name === actualSearchTerm);
+      // if (chosenCity !== undefined){
+        // console.log('not undefined!')
+        // return chosenCity;
+      // }
+    // };
+    
+    // const chosenCity = await suggestions.find(el => el.name === actualSearchTerm);
+    // let chosenCity;
+    // const chooseCity = async (_callback) => {
+      // chosenCity = suggestions.find(el => el.name === actualSearchTerm);
+      // _callback()
+    // }
+    
+    // console.log(newUnits, "search");
+    // dispatchWeather({type: 'WEATHER_FETCH_INIT'});
+    
+
+    // chooseCity()
+      // .then( chosenCity => {
+        // console.log(chosenCity, "asdfasghfr")
+        // if (chosenCity !== undefined){
+          // curr_position = chosenCity.geolocation
+          // getWeather(chosenCity.geolocation, newUnits)
+        // }
+        // return chosenCity;
+      // })
+    // setCurr_position(chosenCity.geolocation);
+    // getWeather(chosenCity.geolocation, newUnits);
+    
+    // const setPosGetWeather = () => {
+      // chooseCity(() => {
+        // setCurr_position(chosenCity.geolocation);
+        // getWeather(chosenCity.geolocation, newUnits);
+      // })
+    // }
+    
+    
   }
 
+  React.useEffect(() => {
+    dispatchWeather({type: 'WEATHER_FETCH_INIT'});
+    getWeather(curr_position, newUnits);
+  }, [curr_position])
+
+  // the array of suggestions, to be displayed as datalist options
   const [suggestions, setSuggestions] = React.useState('');
-  const [searchTerm, setSearchTerm] = React.useState('');
+  // dis-/enabling the search button, since i have to read the geolocation 
+  // from the chosen city before making a weather request. 
+  // the one call api doesn´t seem to support searching for city names.
   const [disableSearch, setDisableSearch] = React.useState(true);
 
   let cancelToken;
 
-  const handleChange = (event) => {
-    if (event.target.value.length > 3) {
+  const handleChange = (e) => {
+    // only making requests if the user has typed more than three letters, to save resources
+    if (e.target.value.length > 3) {
+      // i have to do all this shit because datalist options do not support onclick
+      // (i'm looking if the input target value matches one of the suggestions. if it does, we can click search)
+      const spacePosition = e.target.value.lastIndexOf(' ');
+      const inputChosenCity = e.target.value.substring(0, spacePosition);
+      const cityMatch = suggestions && suggestions.filter(suggestion => suggestion.name === inputChosenCity);
+      // console.log({cityMatch});
+      cityMatch.length && setDisableSearch(false);
+
       if (cancelToken !== undefined) {
         cancelToken.cancel("aborted due to new request");
       }
 
       cancelToken = axios.CancelToken.source();
+      setSearchTerm(e.target.value);
 
-      axios.get(`http://localhost:8000/?query=${event.target.value}`, {cancelToken: cancelToken.token})
+      axios.get(`http://localhost:8000/?query=${e.target.value}`, {cancelToken: cancelToken.token})
       .then(response => {
-        console.log(response.data);
-        setSearchTerm(event.target.value);
+        console.log(response.data, "response")
+        
         if (response.data.length) {
           setSuggestions(response.data);
         }
@@ -138,18 +205,13 @@ function App() {
     }
   }
 
-  const enableSearch = () => {
-    console.log("raccoon")
-    setDisableSearch(!disableSearch)
-  }
-
   return (
     <div className="bg-img">
       <div className="bg-overlay">
         <div className="header-bg">
           <header>
-            <h1>TRVE & KALLT</h1>
-            <Searchbar disableSearch={disableSearch} enableSearch={enableSearch} handleSearch={handleSearch} handleChange={handleChange} suggestions={suggestions} />
+            <h1>TRVE&nbsp;& KALLT</h1>
+            <Searchbar disableSearch={disableSearch} handleSearch={handleSearch} handleChange={handleChange} suggestions={suggestions} />
             <div className="toggle">Metric / Imperial: 
               <Unittoggle onToggle={triggertoggle} temptoggle={temptoggle}/>
             </div>
@@ -189,18 +251,19 @@ const Unittoggle = ({onToggle, temptoggle}) => {
     )
 }
 
-const Searchbar = ({disableSearch, enableSearch, handleSearch, handleChange, suggestions}) => {
-  console.log(disableSearch)
+const Searchbar = ({disableSearch, handleSearch, handleChange, suggestions}) => {
+  console.log({disableSearch})
   return (
-    <form onSubmit={handleSearch}>
-      <label htmlFor="city">City:</label>
+    <form className="searchbar" onSubmit={handleSearch}>
+      <label htmlFor="city" className="citylabel" ><div className="citylabeltext" >City:</div></label>
       <input onChange={handleChange} autoComplete="off" type="text" name="cityinput" id="cityinput" list="suggestions" />
       <datalist id="suggestions">
         <>
-        {suggestions && suggestions.map((suggestion, i) => <option key={i} onClick={enableSearch} value={`${suggestion.name} (${suggestion.country})`} />)}
+        {/* WHY wont this show <option key="f***" value="Loading" /> */}
+        {suggestions && suggestions.map((suggestion, i) => <option key={i} value={`${suggestion.name} ${suggestion.country}`} />)}
         </>
       </datalist>
-      <button className="searchsubmit" disabled={ disableSearch ? true : false } id="search" type="submit">Search</button>
+      <button className="searchsubmit" disabled={ disableSearch } id="search" type="submit"><Searchbtn width="16px" /></button>
     </form>
   )
 }
