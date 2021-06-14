2d1d361cb367c7ec66c7e4ee21593fb75efdab54
johanneslepsius <johannes.lepsius@chasacademy.se>
2021-06-14 22:58:03 +0200

    restyle content in code & css

--- frontend/src/weatherinfo/infocard/Infocard.js
+++ frontend/src/weatherinfo/infocard/Infocard.js
@@ -26,15 +26,9 @@ const Infoitem = ({e, daily, units}) => {
     <div className="infoitem" onClick={() => setExpanded(!expanded)}>
       <div className="infocontent">
         {daily ? (
-          <div className="contentgroup">
             <p><b>{getTime(e.dt, "DAY")}</b></p>
-            <p>{`${Math.round(e.temp.min)} - ${Math.round(e.temp.max)}`}&nbsp;{units.temp}</p>
-          </div>
         ):(
-          <div className="contentgroup">
             <p><b>{getTime(e.dt, "HOUR")}</b></p>
-            <p>{Math.round(e.temp)}&nbsp;{units.temp}</p>
-          </div>
         )}
         <div className="contentgroup">
           <img className="weathericon" src={getUrl(e.weather[0].icon)} alt=""/>
@@ -44,19 +38,24 @@ const Infoitem = ({e, daily, units}) => {
           <p>Wind:&nbsp;{Math.round(e.wind_speed * 3.6)}&nbsp;{units.wind}
             <Arrow height="13px" style={{transform: `rotate(${e.wind_deg}deg`}}/>
           </p>
-          <p>
-            <Raindrop height="13px" />
-            {Math.round(e.pop * 100)}%
-          </p>
+          {daily
+            ? <p>{`${Math.round(e.temp.min)} - ${Math.round(e.temp.max)}`}&nbsp;{units.temp}</p>
+            : <p>{Math.round(e.temp)}&nbsp;{units.temp}</p>
+          }
         </div>
         </div>
       {expanded && <div className="infocontent">
         <p>Humidity:&nbsp;{e.humidity}%</p>
+        <p>
+            <Raindrop height="13px" />
+            {Math.round(e.pop * 100)}%
+          </p>
         {daily && <p>
             <span>Sunrise:&nbsp;{getTime(e.sunrise, "HOUR_MINUTES")}</span><br/>
             <span>Sunset:&nbsp;{getTime(e.sunset, "HOUR_MINUTES")}</span>
           </p>}
       </div>}
+      <hr />
     </div>)
 };
 
