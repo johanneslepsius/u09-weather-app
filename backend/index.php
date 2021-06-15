ac5ee6be46695ac769863f6db96e5297b3314472
johanneslepsius <johannes.lepsius@chasacademy.se>
2021-06-15 22:30:32 +0200

    change city list to smaller file

--- backend/index.php
+++ backend/index.php
@@ -1,16 +1,15 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-$citiesJSON = file_get_contents('./citylist.json');
+$citiesJSON = file_get_contents('./city.list.json');
 $cities = json_decode($citiesJSON, true);
 
 $query = $_GET['query'];
-var_dump($query);
 $citiesResponse = [];
 
 if ($query) {
     foreach ($cities as $city => $value) {
         if (stripos($value["name"], $query) !== false) {
-            array_push($citiesResponse, ['name' => $value["name"], 'country' => $value["country"], 'geolocation' => ['latitude' => $value['coord']['lat'], 'longitude' => $value['coord']['lon']]]);
+            array_push($citiesResponse, ['name' => $value["name"], 'country' => $value["country"], 'geolocation' => ['latitude' => $value['lat'], 'longitude' => $value['lng']]]);
         };
     };
 };
